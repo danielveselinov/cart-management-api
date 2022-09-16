e3f21b45eba8508937df429c569c95ccf40a0c11
Daniel Veselinov <daniel@veselinov.com>
2022-09-16 11:13:31 +0200

    Category API tests

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -9,27 +9,52 @@ use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    // use RefreshDatabase;
     // category.index - category.store - category.show - category.update - category.destory
 
     public function test_get_categories()
     {
-        // Category::create(['name' => 'category']);
-
         $response = $this->getJson(route('category.index'));
+        $response->assertJson([]);
 
-        $this->assertEquals(1, count($response->json()));
+        $response->assertStatus(202);
     }
 
-    // public function test_delete_category()
-    // {
-    //     $category = Category::factory()->create(['name' => 'category']);
+    public function test_store_a_category()
+    {
+        $response = $this->postJson(route("category.store"), [
+            'name' => 'category_test'
+        ]);
+        $response->assertJson([]);
 
-    //     $this->deleteJson(route('category.destroy', $category->id))
-    //             ->assertNoContent();
-        
-    //     $this->assertDatabaseMissing('categories', [
-    //         'name' => $category->name
-    //     ]);
-    // }
+        $response->assertStatus(202);
+    }
+
+    public function test_show_a_category()
+    {
+        $category = Category::where('name', '=', 'category_test')->first();
+        $reesponse = $this->getJson(route('category.show', $category->id));
+        $reesponse->assertDontSee([]);
+
+        $reesponse->assertStatus(202);
+    }
+
+    public function test_update_a_category()
+    {
+        $category = Category::where('name', '=', 'category_test')->first();
+        $response = $this->putJson(route('category.update', $category->id), [
+            'name' => 'category_updated'
+        ]);
+        $response->assertDontSee([]);
+
+        $response->assertStatus(202);
+    }
+
+    public function test_destory_a_category()
+    {
+        $category = Category::where('name', '=', 'category_updated')->first();
+        $response = $this->deleteJson(route('category.destroy', $category->id));
+        $response->assertNoContent();
+
+        $response->assertStatus(204);
+    }
 }
