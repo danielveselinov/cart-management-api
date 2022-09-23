e64d83cf299ad4cb02d60a43a4fed57babac20dd
Daniel Veselinov <daniel@veselinov.com>
2022-09-23 11:16:20 +0200

    Registration tests

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -2,18 +2,56 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_example()
+    public function test_new_user_can_register()
     {
-        $response = $this->get('/');
+        $response = $this->postJson(route('auth.register'), [
+            'name' => 'John Doe',
+            'email' => 'john@email.com',
+            'phone_number' => '07550000',
+            'password' => Hash::make('password'),
+            'verification_code' => rand(1000, 9999),
+        ])->assertCreated();
 
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', $response->json());
+    }
+
+    public function test_if_user_try_existing_email_for_registration_throws_an_error()
+    {
+        $user = User::factory()->create();
+
+        $this->postJson(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'phone_number' => $user->phone_number,
+            'password' => Hash::make($user->password),
+            'verification_code' => rand(1000, 9999),
+        ])->assertUnprocessable();
+    }
+
+    public function test_if_user_try_existing_phone_number_for_registration_throws_an_error()
+    {
+        $user = User::factory()->create();
+
+        $this->postJson(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'phone_number' => $user->phone_number,
+            'password' => Hash::make($user->password),
+            'verification_code' => rand(1000, 9999),
+        ])->assertUnprocessable();
+    }
+
+    public function test_if_user_left_any_empty_fields_throws_an_errors()
+    {
+        $this->postJson(route('auth.register'))->assertUnprocessable();
     }
 }
