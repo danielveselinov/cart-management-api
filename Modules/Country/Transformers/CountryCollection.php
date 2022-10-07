6533954b5a38ba77e58c1ac72825283788ec55c7
Daniel Veselinov <daniel@veselinov.com>
2022-10-07 16:24:56 +0200

    swagger country init

--- Modules/Country/Transformers/CountryCollection.php
+++ Modules/Country/Transformers/CountryCollection.php
@@ -4,10 +4,34 @@ namespace Modules\Country\Transformers;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
+/**
+ * @OA\Schema(
+ *      title="CountryCollection",
+ *      description="Country collection"
+ * ),
+ */
 class CountryCollection extends ResourceCollection
 {
-    /**
+     /**
      * Transform the resource collection into an array.
+     * 
+     * @OA\Property(
+     *      property="countries",
+    *       description="Country Collection",
+    *       type="array",
+    *       @OA\Items(
+    *           ref="#/components/schemas/CountryResource"
+    *       )
+     * ),
+     * @OA\Property(
+     *      property="pagination",
+     *      type="object",
+     *      @OA\Property(property="total", type="integer", ),
+     *      @OA\Property(property="count", type="integer", ),
+     *      @OA\Property(property="per_page", type="integer", ),
+     *      @OA\Property(property="current_page", type="integer", ),
+     *      @OA\Property(property="total_page", type="integer")
+     * )
      *
      * @param  \Illuminate\Http\Request
      * @return array
--- Modules/Country/Transformers/CountryResource.php
+++ Modules/Country/Transformers/CountryResource.php
@@ -4,10 +4,20 @@ namespace Modules\Country\Transformers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *      title="CountryResource",
+ *      description="Country resource",
+ * )
+ */
 class CountryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     * 
+     * @OA\Property(property="name", type="string", ),
+     * @OA\Property(property="key", type="string", ),
+     *
      *
      * @param  \Illuminate\Http\Request
      * @return array
